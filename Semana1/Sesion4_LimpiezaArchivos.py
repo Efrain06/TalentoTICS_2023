bc46db03cee5a2652c3e97b08f3b4833f9146e15
Efrain06 <65642344+Efrain06@users.noreply.github.com>
2023-11-23 17:30:24 -0500

    Update Sesion4_LimpiezaArchivos.py

--- Semana1/Sesion4_LimpiezaArchivos.py
+++ Semana1/Sesion4_LimpiezaArchivos.py
@@ -3,11 +3,14 @@
 # PRONTO EMPEZAMOS, GRACIAS POR LA ESPERA
 
 import pandas as pd
+import numpy as np
 
  #Problemas típicos en un dataframe (asumiendo que el archivo se cargue de forma correcta):
-    # 1. datos nulos o faltantes
-    # 2. filas/datos duplicadas
-    # 3. tipos de datos incorrectos
+    # 1. datos nulos o faltantes (a veces, columnas completas)
+    # 2. filas/datos duplicados
+    # 3. datos incorrectos
+    #   3.1 formato de datos
+    #   3.2 valor/rango de datos
 
 
     # 4. índices incorrectos
@@ -23,7 +26,7 @@ df_air_quality.info()
 #----- 1. datos nulos o faltanes en un dataframe:
 
 #se identificó una columna donde todos los valores son nulos, descartar columna
-# se usa la función drop : nombre de la columna, elimina en el mismo dataframe,
+# se usa la función drop : nombre de la columna, elimina en el mismo dataframe, 
 # axis corresponde al eje del dataframe: axis = 1, corresponde a las columnas (eje vertical)
 #                                       axis = 0, corresponde a las filas (eje horizontal)
 df_air_quality.drop("Message",inplace=True,axis=1)
@@ -33,7 +36,7 @@ df_air_quality.info()
 df_air_q_clean1 = df_air_quality.dropna()
 df_air_q_clean1.info()
 
-#---------- 2. Verificar Duplicados
+#---------- 2. Verificar Duplicados 
 #mostrar qué filas están duplicadas:
 print(df_air_q_clean1.duplicated())
 
@@ -41,8 +44,44 @@ df_dup = df_air_q_clean1[df_air_q_clean1.duplicated()]
 print(df_dup.info())
 print(df_dup)
 
+
 #eliminar filas duplicadas del dataframe:
 df_air_q_clean1.drop_duplicates(inplace=True)
 
 #----------- 3. Corregir formato de datos
+df_meteo = pd.read_csv("Meteorite_Landings_Dirty.csv")
+df_meteo.info()
 
+#eliminar valores nulos:
+df_meteo.dropna(inplace=True)
+df_meteo.info()
+
+#eliminar duplicados:
+print(df_meteo.duplicated())
+
+df_meteo_dup = df_meteo[df_meteo.duplicated()]
+print(df_meteo_dup)
+
+df_meteo.drop_duplicates(inplace=True)
+df_meteo.info()
+
+# (!!!!!) ¿CÓMO IDENTIFICAR ESTE TIPO DE ERRORES?
+# ESTRATEGIA: Identificar de antemano cuáles son las columnas importantes en las tareas de análisis de datos
+# y proceder a "corregirlas" o "analizarlas"
+# validando el rango o encontrando datos atípicos
+# 3.1 eliminar errores de formato de datos (cuando se identifica columna específica):
+df_meteo = df_meteo.astype({"year":"int64"})
+
+print(df_meteo.head(10).to_string())
+
+
+# 3.2 corregir errores de valores / rango de datos (identificar las filas en las que suceden)
+#     se debe iterar en el dataframe para la columna que quiero corregir o validad:
+for i,item in df_meteo.head(15).iterrows():
+    if item["year"] < 1900 or item["year"] > 2023:
+        print(item)
+        #corregir el año:
+        df_meteo.loc[i,"year"] = 1900
+
+
+print(df_meteo.head(15))
