d10fd97a13f0e33ea587bb76e2a24f652faaa311
Efrain06 <65642344+Efrain06@users.noreply.github.com>
2023-11-23 21:02:09 -0500

    Add files via upload

---
+++ Semana1/Sesion5_ActividadPractica(Carga y Limpieza).py
@@ -0,0 +1,22 @@
+# PRACTICA WORKLOW ANALISIS DE DATOS
+# QUE ES EL ANALISIS DE DATOS?
+# PROCESO SISTEMATICO DE APLICAR TECNICAS DE CARGA, LIMPIEZA, ANALISIS ESTADISTICO DE DATOS
+# EVALUAR Y APOYAR LA TOMA DE DECISIONES
+
+
+# WORKLOW DA:
+# 1. CARGA DE DATOS -> 2. LIMPIEZA DE DATOS -> 3. ANÁLISIS -> 4. VISUALIZACIÓN
+#
+# (a) Determinar cuales son los errores posibles / necesidades de analisis
+#              ¿Cuales son las columnas importantes en la fase 3 del Worklow?
+#              ¿Identificar cuales osn los posibles errores presentes en esas columnas
+#              -> Formato de datos (numerico, real o entero, cadena de caracteres): astype
+#              -> Rango de datos ( Validacion de acuerdo a los valores que se requieren)
+#                     ej: Edad de personas no admiten numeros negativos, rango de años, meses
+#              Iterar sobre el dataframe y analizar cada una de las columnas importantes
+#
+# (b) Determinar como se van a corregir
+#              -> Eliminacion de filas con valores nulos: drop, dropna
+#              -> Eliminacion de columnas nulas: drop
+#              -> Eliminacion de duplicados: drop_duplicated
+
