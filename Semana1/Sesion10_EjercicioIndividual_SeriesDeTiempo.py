2c4d6386615f73e1b2854c8280fcc86692960048
Efrain06 <ev140311@gmail.com>
2023-12-01 18:21:47 -0500

    Update Sesion10_EjercicioIndividual_SeriesDeTiempo.py

--- Semana1/Sesion10_EjercicioIndividual_SeriesDeTiempo.py
+++ Semana1/Sesion10_EjercicioIndividual_SeriesDeTiempo.py
@@ -1,22 +1,24 @@
-# Talento TICs 2023
-# Pyton 2
+#Talento TICs 2023
+#Python 2
 
-# Relacionando dos datasets
-# Concatenacion y merge de dataset
+#Relacionando dos datasets
 import pandas as pd
 import matplotlib.pyplot as plt
 
+#concatenación y merge de datasets:
+
+#
 sales_data_pts = {
     "point_id": [1,2,3,4,5,6,7,8],
-    "nombres": ["A", "B", "C", "D", "E", "F", "G", "H"],
-    "sales": [ 100.1, 200.0, 209.0, 420.0, 400.0, 250.0, 120.0, 88.0,]
+    "nombres": ["Centro","Sur","Norte","Oriente","Occidente","Sur 2","Norte 2","Centro 2"],
+    "sales_pt" : [  100.0, 200.00, 300.00,150.00,400.00,90.00,120.00,88.00 ]
 }
 
 sales_data_prod = {
     "point_id": [1,2,3,4,5,6,7,8],
-    "prod_id": [11,23,31,44,55,86,37,18],
-    "nomb_prod": ["l", "f", "s", "d", "n", "b", "k", "j"],
-    "vlr_unit": [ 10.0, 20.0, 29.0, 42.0, 4.0, 22.0, 12.0, 8.0,]
+    "prod_id": [11,12,13,11,21,11,30,21],
+    "prod": ["Zapatos A","Zapatos B","Zapatos C","Zapatos D","Zapatos E","Zapatos F","Zapatos G","Zapatos H"],
+    "sls_prd" : [  20.0, 15.00, 10.00,12.00,9.00,11.00,22.00,18.5 ]
 }
 
 df_sales_pt = pd.DataFrame(sales_data_pts)
@@ -25,11 +27,11 @@ df_sales_prod = pd.DataFrame(sales_data_prod)
 print(df_sales_pt)
 print(df_sales_prod)
 
-# El merge, mezcla dos dataset, dataframe a partir de una columna comun;
-# No importa que tengan distinto # de filas
-df_all_data = pd.merge(df_sales_pt, df_sales_prod, on = "point_id")
+#el merge, mezcla dos datasets/dataframes con distintas columnas, a partir de una columna común
+#no importa que tengan distinto # de filas
+df_all_data = pd.merge(df_sales_pt, df_sales_prod,on="point_id")
 
-df_group = df_all_data.groupby("prod_id")["vlr_unit"].mean().plot(kind="bar")
+#podemos operar con el nuevo dataset de acuerdo a lo visto hasta ahora:
+df_group = df_all_data.groupby("prod")["sls_prd"].sum().plot(kind="bar")
 plt.show()
-print(df_all_data)
-
+print(df_group)
